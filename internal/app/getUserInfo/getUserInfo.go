ead32e2eeb2cdd8d585bf6f647108f601fd6216e
ada2 <87185550+2ADA2@users.noreply.github.com>
2025-02-18 23:33:54 +0300

    uniback v 0.3.5

--- internal/app/getUserInfo/getUserInfo.go
+++ internal/app/getUserInfo/getUserInfo.go
@@ -2,7 +2,6 @@ package getUserInfo
 
 import (
 	"context"
-	"encoding/json"
 	"myapp/internal/app/models"
 	"myapp/internal/app/responses"
 	"myapp/internal/pkg/api"
@@ -27,20 +26,10 @@ var userCfgCollection *mongo.Collection = api.GetCollection(api.DB, "configs")
 func (e *GetUserInfo) Status(c echo.Context) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-
-	var jsonUser models.UserInfo
-	err := json.NewDecoder(c.Request().Body).Decode(&jsonUser)
-
-	if err != nil {
-		return c.JSON(http.StatusNotFound, responses.UserResponse{
-			Status:  http.StatusNotFound,
-			Message: "notFound",
-			Data:    &echo.Map{"data": "no such data"},
-		})
-	}
+	name := c.QueryParam("name")
 
 	var user models.User
-	usersCollection.FindOne(ctx, bson.M{"name": jsonUser.User}).Decode(&user)
+	usersCollection.FindOne(ctx, bson.M{"name": name}).Decode(&user)
 
 	if user.Name == "" {
 		return c.JSON(http.StatusNotFound, responses.UserResponse{
@@ -57,7 +46,14 @@ func (e *GetUserInfo) Status(c echo.Context) error {
 		Status:  http.StatusCreated,
 		Message: "success",
 		Data: &echo.Map{
-			"data": userCfg,
+			"data": map[string]interface{}{
+				"User":       userCfg.User,
+				"About":      userCfg.About,
+				"Followers":  userCfg.Followers,
+				"Subscribes": userCfg.Subscribes,
+				"Links":      userCfg.Links,
+				"Date":       userCfg.Date,
+			},
 		},
 	})
 }
